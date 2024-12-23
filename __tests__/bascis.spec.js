121c0f3f5870d57c2d911c4c147774cb01f20122
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-23 10:08:32 +0300

    Test Null Values

--- __tests__/bascis.spec.js
+++ __tests__/bascis.spec.js
@@ -4,6 +4,12 @@ test('addition', ()=> {
     expect(2+2).toBe(4);
 });
 
-test('addition false', ()=>{
-    expect(2+2).toBe(3);
-})
+// test('addition false', ()=>{
+//     expect(2+2).toBe(3);
+// });
+
+test('Test Null', () => {
+    const i = null;
+
+    expect(i).toBeNull();
+});
